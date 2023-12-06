9fa07100505a0c945db86cca14e746c79d6ef89d
undefined undefined <23LP5760@G303-CC0140.local>
2023-12-06 11:13:02 +0800

    metablog

--- MetaBlog/metablog/src/components/icons/MainBigPic.jsx
+++ MetaBlog/metablog/src/components/icons/MainBigPic.jsx
@@ -3,8 +3,6 @@ export default function (props) {
     <div
       className="w-[1216px] h-[600px] bg-cover flex"
       style={{ backgroundImage: `url(${props.img})` }}
-    >
-      <h1>ffff</h1>
-    </div>
+    ></div>
   );
 }
