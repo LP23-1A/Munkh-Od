c289dca7c025737a30c0590ab8d89c2e95daa322
Munkh-Od Batsukh <23LP5760@G303-CC0146.local>
2023-12-19 11:30:54 +0800

    nodejs

--- databaselesson/index.js
+++ databaselesson/index.js
@@ -1,11 +1,53 @@
 const express = require("express");
+const bp = require("body-parser");
 const app = express();
-const port = 3000;
+const port = 8000;
+
+app.use(bp.json());
+
+const users = [
+  {
+    id: 1,
+    name: "orgil",
+  },
+  {
+    id: 2,
+    name: "naki",
+  },
+  {
+    id: 3,
+    name: "munkherdene",
+  },
+];
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send({ success: true, users: users }).end;
+});
+
+app.post("/", (req, res) => {
+  res.send({ success: true, users: users }).end();
+});
+
+app.put("/:id", (req, res) => {
+  const id = req.params.id;
+
+  users.map((user) => {
+    if (user.id === parseInt(id)) {
+      console.log(id);
+      user.name = req.body.name;
+    }
+  });
+
+  res.send({ success: true, method: users }).end();
+});
+
+app.delete("/:id", (req, res) => {
+  const id = req.params.id;
+  const deletedUserId = users.findIndex((user) => user.id === parseInt(id));
+  users = users.slice(o, deletedUserId);
+  res.send({ success: true, users: users }).end;
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log("Server running");
 });
