b4f3e2f1d2aa923d84ffc2fc04a7d3cf12eb7e09
Munkh-Od Batsukh <23LP5760@G303-CC0146.local>
2024-01-23 12:39:01 +0800

    todo

--- todo/backend/src/index.ts
+++ todo/backend/src/index.ts
@@ -1,14 +1,18 @@
 import express from "express";
 import { connectDatabase } from "./utils/database";
+import { auth } from "./router/User";
 
 const PORT = 8000;
 
 const start = () => {
   const app = express();
+  app.use(express.json());
 
   connectDatabase();
 
-  app.get("/", (reg, res) => {
+  app.use("/auth", auth);
+
+  app.get("/", (req, res) => {
     res.status(200).send({ success: true, msg: "Hello world" });
   });
 
